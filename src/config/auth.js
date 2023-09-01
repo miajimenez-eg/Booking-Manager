d6d2d4f571daf4e8dba05b5cae3d8aa5cb744af0
Mia Jimenez <miajimenez@expediagroup.com>
2023-09-01 10:58:52 +0100

    remove sendgrid api environment variable

--- src/config/auth.js
+++ src/config/auth.js
@@ -4,8 +4,7 @@ const authConfig = {
     baseURL: process.env.AUTH0_AUDIENCE,
     clientID: process.env.AUTH0_CLIENT_ID,
     issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-    secret: process.env.AUTH0_SECRET,
-    sendGridApiKey: process.env.SENDGRID_API_KEY
+    secret: process.env.AUTH0_SECRET
 }
 
 module.exports = authConfig;
